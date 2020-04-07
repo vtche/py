4821fe4c8b0842ebe9cdc87b6a50dd96e4f7707b
vtche <someone@somewhere.net>
2020-04-07 01:04:01 -0400

    added instructions and env

--- README.md
+++ README.md
@@ -2,5 +2,11 @@
 python test
 
 ## Some update here 
+First install pipenv
+`pip install pipenv`
 
-
+Then run 
+```
+pipenv install
+pipenv run meteors.py
+```
--- meteor.py
+++ meteor.py
@@ -1,2 +1,7 @@
 # -*- coding: utf-8 -*-
+import math
+import requests
 
+
+url = "https://data.nasa.gov/resource/gh4g-9sfh.json"
+data = requests.get(url).json()
